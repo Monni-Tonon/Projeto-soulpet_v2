6b572bd46d45348e66a3377f2903478d6a61ede4
Monni-Tonon <monnicy@gmail.com>
2023-05-06 12:17:18 -0300

    projeto soulpet-v2

---
+++ soulpet-back/database/pet.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+const Cliente = require("./cliente");
+
+const Pet = connection.define("pet", {
+  nome: {
+    type: DataTypes.STRING(130),
+    allowNull: false,
+  },
+  tipo: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  porte: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  dataNasc: {
+    type: DataTypes.DATEONLY,
+  },
+  imagem: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
+});
+
+// Relacionamento 1:N (Um cliente pode ter N pets)
+Cliente.hasMany(Pet, { onDelete: "CASCADE" });
+// CASCADE = quando o cliente for deletado, TODOS os pets serão deletados
+Pet.belongsTo(Cliente); // Um pet pertece a um cliente
+
+module.exports = Pet;
